e4845aa3d22fed22404946f37bb1ff953cbdf28b
pn lin <245810159@qq.com>
2020-05-20 18:32:19 +0800

    dev

--- src/layout/components/Header/index.vue
+++ src/layout/components/Header/index.vue
@@ -3,19 +3,18 @@
     <div class="container">
       <div class="logo">
         <router-link to="/">
-          <img src="../../../assets/images/logo.png" alt="狐狸小说" />
+          <img src="../../../assets/images/logo.png" alt="狐狸小说" width="40" />
+          <span>作者专区</span>
         </router-link>
       </div>
       <div class="right">
         <router-link to="/">
-          <img src="../../../assets/images/huli_09.png" alt="作者福利" />
-          作者福利
+          <img src="../../../assets/images/fuli.png" alt="作者福利" width="18" />作者福利
         </router-link>
         <router-link to="/message">
-          <img src="../../../assets/images/huli_11.png" alt="消息" />
+          <img src="../../../assets/images/message.png" alt="消息" width="18" />
           <span>消息</span>
           <el-badge :value="9" class="mark" />
-
         </router-link>
         <router-link to="/">
           <div class="avatar">
